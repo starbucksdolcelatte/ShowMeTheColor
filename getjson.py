74e8072f13d3196e227fad22cea07ce7755e17de
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-05-27 10:08:17 +0900

    add list []

--- getjson.py
+++ getjson.py
@@ -1,12 +1,13 @@
 import json
+from collections import OrderedDict
 
 class GetJson:
     def get_standard(self, filename):
         with open(filename, mode='r') as f:
-            json_data = json.load(f)
+            json_data = json.load(f, object_pairs_hook=OrderedDict)
 
         # 아래와 같은 계절별 기준값이 들어갈 리스트
-        result_list = [[[],[]],[[],[]],[[],[]],[[],[]]]
+        result_list = [[[],[],[],[]],[[],[],[],[]],[[],[],[],[]],[[],[],[],[]]]
         '''
         spr[standard_1[skin[R,G,B], hair[R,G,B], eye[R,G,B]],
             ...
@@ -24,10 +25,14 @@ class GetJson:
         i = 0
         j = 0
         for season in json_data:
+            print(season)
             for std in json_data[season]:
+                print(std)
                 for body_part in json_data[season][std]:
+                    print(body_part)
                     result_list[i][j].append(json_data[season][std][body_part])
                 j += 1
             i += 1
             j = 0
+
         return result_list
